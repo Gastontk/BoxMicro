25cea5fab0d63fd075db61ece9824165152f55f7
gtktext <gtktext@gmail.com>
2019-05-17 15:46:10 -0700

    setup server to serve build directory

--- boxmicro/server/index.js
+++ boxmicro/server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/boxmicro");
+
+app.use(express.static(path.join(__dirname, "../build")));
 
 const Todo = mongoose.model("Todo", {
 	text: String,
@@ -18,6 +21,10 @@ const Polygon = mongoose.model("Polygon", {
 // app.get("/working", (req, res) => {
 // 	res.send("this route is now working. You have reached /working");
 // });
+
+app.get("/*", function(req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 mongoose.connection.once("open", function() {
 	app.listen(4000, () => console.log("Server is running on localhost:4000"));
